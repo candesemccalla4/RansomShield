89f6032446853be1927d2ed5d8e81b9d25fbbad5
Candese <cmccalla1@mercy.edu>
2026-07-29 15:25:07 -0400

    Add ransomware protection scoring engine

---
+++ app/app/scoring_engine.py
@@ -0,0 +1,27 @@
+def calculate_score(
+    antivirus,
+    backup,
+    mfa,
+    monitoring
+):
+
+    score = 0
+
+
+    if antivirus:
+        score += 20
+
+
+    if backup:
+        score += 30
+
+
+    if mfa:
+        score += 20
+
+
+    if monitoring:
+        score += 30
+
+
+    return score
