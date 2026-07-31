43b990df1c02449fedac4107ea95c918c7314bd5
Candese <cmccalla1@mercy.edu>
2026-07-31 12:31:00 -0400

    Add ransomware alert system

---
+++ detection/alerts.py
@@ -0,0 +1,17 @@
+def send_alert(message):
+
+    print("="*40)
+
+    print("RansomShield ALERT")
+
+    print(message)
+
+    print("="*40)
+
+
+
+if __name__ == "__main__":
+
+    send_alert(
+        "Possible ransomware activity detected"
+    )
