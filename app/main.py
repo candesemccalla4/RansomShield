513adbe056bf4b93679d29a3c518f1f05f28298f
Candese <cmccalla1@mercy.edu>
2026-07-31 13:21:52 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
 """
 RansomShield Main Controller
 
